50f3c3ee4d89454f2a89d4866f0e0b8aa2c64775
kapilyaswanth <kapilyaswanthsb@gmail.com>
2023-05-06 20:26:30 +0530

    learnt basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  def hello
+    render html: "hello world"
+  end
+
+  def goodbye
+    render html: "good bye, world!"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index", as: :about_custom
   #if get "demo/about" can be accessed as demo_about_path in erb for redirections
-  root to:"main#index"
+  root to:"application#goodbye"
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "sign_up", to: "registration#new", as: :sign_up_path
-  post "sign_up", to: "registration#create"
+  # get "sign_up", to: "registration#new", as: :sign_up_path
+  # post "sign_up", to: "registration#create"
 end
