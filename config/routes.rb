0fa6985d87b44682f3564bfeed5d9275d8475cae
kapilyaswanth <kapilyaswanthsb@gmail.com>
2023-05-20 13:34:04 +0530

    working with views yiel provide partials layout tests for controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+
   resources :microposts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index", as: :about_custom
   #if get "demo/about" can be accessed as demo_about_path in erb for redirections
-  root to:"application#goodbye"
+  # root to:"application#goodbye"
+  root "static_pages#home"
   # Defines the root path route ("/")
   # root "articles#index"
 
